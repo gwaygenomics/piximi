026d09c092b6ff529ad11bd7140279ae9cd06880
Allen Goodman <allen.goodman@icloud.com>
2019-12-16 18:22:23 -0500

    ClassifierForm

--- packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/ClassifierListItem/ClassifierForm/ClassifierForm/ClassifierForm.tsx
+++ packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/ClassifierListItem/ClassifierForm/ClassifierForm/ClassifierForm.tsx
@@ -1,9 +1,36 @@
 import Grid from "@material-ui/core/Grid";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
 import * as React from "react";
 
 import {useStyles} from "./ClassifierForm.css";
 
+const losses = [
+  ["absoluteDifference", "Absolute difference"],
+  ["cosineDistance", "Cosine distance"],
+  ["hingeLoss", "Hinge"],
+  ["huberLoss", "Huber"],
+  ["logLoss", "Log"],
+  ["meanSquaredError", "Mean squared error (MSE)"],
+  ["sigmoidCrossEntropy", "Sigmoid cross entropy"],
+  ["categoricalCrossentropy", "Categorical cross entropy"]
+];
+
 type ClassifierFormProps = {};
+
+const LossFunctionTextField = () => {
+  return (
+    <TextField select>
+      {losses.map(([k, v], index) => {
+        return (
+          <MenuItem dense key={index} value={k}>
+            {v}
+          </MenuItem>
+        );
+      })}
+    </TextField>
+  );
+};
 
 export const ClassifierForm = ({}: ClassifierFormProps) => {
   const classes = useStyles({});
@@ -11,24 +38,48 @@ export const ClassifierForm = ({}: ClassifierFormProps) => {
   return (
     <form className={classes.container}>
       <Grid container spacing={2}>
-        goo
+        <Grid item xs={4}>
+          <LossFunctionTextField />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <TextField
+            id="input-shape"
+            label="Input shape"
+            className={classes.textField}
+            value={""}
+            disabled
+            margin="normal"
+          />
+        </Grid>
+
+        <Grid item xs={2}>
+          <TextField
+            id="batch-size"
+            label="Batch size"
+            className={classes.textField}
+            value={32}
+            type="number"
+            margin="normal"
+          />
+        </Grid>
+
+        <Grid item xs={2}>
+          <TextField
+            id="epochs"
+            label="Epochs"
+            className={classes.textField}
+            value={10}
+            margin="normal"
+            type="number"
+          />
+        </Grid>
       </Grid>
     </form>
   );
 };
-
-//
-// const lossFunctions = {
-//   absoluteDifference: "Absolute difference",
-//   cosineDistance: "Cosine distance",
-//   hingeLoss: "Hinge",
-//   huberLoss: "Huber",
-//   logLoss: "Log",
-//   meanSquaredError: "Mean squared error (MSE)",
-//   sigmoidCrossEntropy: "Sigmoid cross entropy",
-//   categoricalCrossentropy: "Categorical cross entropy"
-// };
-//
 
 //
 // type ClassifierFormProps = {
