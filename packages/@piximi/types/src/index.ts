7b9282015bef3660c886e1c3cb944c7beee47a2c
Allen Goodman <allen.goodman@icloud.com>
2019-12-17 13:12:29 -0500

    added model types

--- packages/@piximi/types/src/index.ts
+++ packages/@piximi/types/src/index.ts
@@ -1,22 +1,37 @@
-type Category = {
+import * as tensorflow from "@tensorflow/tfjs";
+
+export type Category = {
   description: string;
   identifier: string;
   index: number;
   visualization: CategoryVisualization;
 };
 
-type CategoryVisualization = {
+export type CategoryVisualization = {
   color: string;
   visible: boolean;
 };
 
-type Classifier = {
-  categories: Category[];
-  images: Image[];
+export type Classifier = {
+  categories: Array<Category>;
+  compileOptions?: CompileOptions;
+  fitOptions?: FitOptions;
+  images: Array<Image>;
   name: string;
 };
 
-type Image = {
+export type CompileOptions = {
+  loss: Loss | Array<Loss> | {[outputName: string]: Loss};
+  metrics: Metric | Array<Metric> | {[outputName: string]: Metric};
+  optimizer: Optimizer;
+};
+
+export type FitOptions = {
+  epochs: number;
+  initialEpoch: number;
+};
+
+export type Image = {
   categoryIdentifier: string;
   checksum: string;
   data: string;
@@ -26,37 +41,64 @@ type Image = {
   visualization: ImageVisualization;
 };
 
-type ImageVisualization = {
+export type ImageVisualization = {
   brightness: number;
   contrast: number;
   visible: boolean;
   visibleChannels: number[];
 };
 
-enum Partition {
+export enum Loss {
+  BinaryCrossentropy = "binaryCrossentropy",
+  CategoricalCrossentropy = "categoricalCrossentropy",
+  CategoricalHinge = "categoricalHinge",
+  CosineProximity = "cosineProximity",
+  Hinge = "hinge",
+  KullbackLeiblerDivergence = "kullbackLeiblerDivergence",
+  Logcosh = "logcosh",
+  MeanAbsoluteError = "meanAbsoluteError",
+  MeanAbsolutePercentageError = "meanAbsolutePercentageError",
+  MeanSquaredError = "meanSquaredError",
+  MeanSquaredLogarithmicError = "meanSquaredLogarithmicError",
+  Poisson = "poisson",
+  SparseCategoricalCrossentropy = "sparseCategoricalCrossentropy",
+  SquaredHinge = "squaredHinge"
+}
+
+export enum Metric {
+  BinaryAccuracy = "binaryAccuracy",
+  CategoricalAccuracy = "categoricalAccuracy",
+  CategoricalCrossentropy = "categoricalCrossentropy",
+  Cosine = "cosine",
+  MAE = "MAE",
+  MAPE = "MAPE",
+  MSE = "MSE",
+  Precision = "precision",
+  SparseCategoricalCrossentropy = "sparseCategoricalCrossentropy"
+}
+
+export enum Optimizer {
+  Adadelta = "Adadelta",
+  Adagrad = "Adagrad",
+  Adam = "Adam",
+  Adamax = "Adamax",
+  RMSProp = "RMSProp",
+  SGD = "SGD"
+}
+
+export enum Partition {
   Training,
   Validation,
   Test
 }
 
-type Score = {
+export type Score = {
   categoryIdentifier: string;
   probability: number;
 };
 
-type Settings = {
+export type Settings = {
   spinner: {
     spinning: boolean;
   };
-};
-
-export {
-  Category,
-  CategoryVisualization,
-  Classifier,
-  Image,
-  ImageVisualization,
-  Partition,
-  Score,
-  Settings
 };
