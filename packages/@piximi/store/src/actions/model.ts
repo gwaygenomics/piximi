11a53f2c4161de2c7d22167074e2aad2abc0e15e
Allen Goodman <allen.goodman@icloud.com>
2019-12-30 16:08:21 -0500

    model actions

--- packages/@piximi/store/src/actions/model.ts
+++ packages/@piximi/store/src/actions/model.ts
@@ -1,17 +1,20 @@
 import {createAction} from "@reduxjs/toolkit";
-import {CompileOptions, FitOptions} from "@piximi/types";
+import {Category, CompileOptions, FitOptions, Image} from "@piximi/types";
 
-export const compile = createAction<any>("MODEL/COMPILE");
+export const compile = createAction<{}>("MODEL/COMPILE");
 
-export const evaluate = createAction<any>("MODEL/EVALUATE");
+export const evaluate = createAction<{}>("MODEL/EVALUATE");
 
-export const fit = createAction<any>("MODEL/FIT");
+export const fit = createAction<{
+  categories: Array<Category>;
+  images: Array<Image>;
+}>("MODEL/FIT");
 
-export const load = createAction<any>("MODEL/LOAD");
+export const load = createAction<{}>("MODEL/LOAD");
 
-export const predict = createAction<any>("MODEL/PREDICT");
+export const predict = createAction<{}>("MODEL/PREDICT");
 
-export const save = createAction<any>("MODEL/SAVE");
+export const save = createAction<{}>("MODEL/SAVE");
 
 export const updateCompileOptions = createAction<CompileOptions>(
   "MODEL/UPDATE-COMPILE-OPTIONS"
