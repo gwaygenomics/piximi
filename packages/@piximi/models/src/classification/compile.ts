b9d55a99787494bb29d2cb05948d72690c7932a6
Allen Goodman <allen.goodman@icloud.com>
2020-01-02 13:40:50 -0500

    model functions

--- packages/@piximi/models/src/classification/compile.ts
+++ packages/@piximi/models/src/classification/compile.ts
@@ -1,4 +1,47 @@
 import * as tensorflow from "@tensorflow/tfjs";
 import {CompileOptions} from "@piximi/types";
 
-export const compile = (options: CompileOptions) => {};
+export const compile = (
+  model: tensorflow.LayersModel,
+  activation: string,
+  classes: number,
+  units: number = 100,
+  options: CompileOptions
+) => {
+  const truncated = tensorflow.model({
+    inputs: model.inputs,
+    outputs: model.output
+  });
+
+  const a = tensorflow.layers.flatten({
+    inputShape: truncated.outputs[0].shape.slice(1)
+  });
+
+  const b = tensorflow.layers.dense({
+    units: units,
+    activation: "relu",
+    kernelInitializer: "varianceScaling",
+    useBias: true
+  });
+
+  const c = tensorflow.layers.dense({
+    units: classes,
+    kernelInitializer: "varianceScaling",
+    useBias: false,
+    activation: "softmax"
+  });
+
+  const config = {
+    layers: [...truncated.layers, a, b, c]
+  };
+
+  const graph = tensorflow.sequential(config);
+
+  graph.compile({
+    optimizer: options.optimizationFunction,
+    metrics: options.metrics,
+    loss: options.lossFunction
+  });
+
+  return graph;
+};
