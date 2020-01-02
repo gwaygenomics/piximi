0b8dfa45ac328bb97e27c6f6d0ef8d2e1aebf984
Allen Goodman <allen.goodman@icloud.com>
2020-01-02 16:43:07 -0500

    model functions

--- packages/@piximi/models/src/classification/fit.ts
+++ packages/@piximi/models/src/classification/fit.ts
@@ -1,13 +1,21 @@
-import {FitOptions} from "@piximi/types";
+import {Category, FitOptions, Image} from "@piximi/types";
 import * as tensorflow from "@tensorflow/tfjs";
 
 export const fit = async (
+  categories: Array<Category>,
+  images: Array<Image>,
   graph: tensorflow.LayersModel,
   options: FitOptions
 ) => {
   const x = tensorflow.randomNormal([100, 224, 224, 3]);
-  let y = tensorflow.randomUniform([100], 0, 9, "int32");
-  y = tensorflow.oneHot(y, 10);
+
+  const categoryIdentifiers = images.map((image: Image) => {
+    return categories.findIndex((category: Category) => {
+      return category.identifier === image.categoryIdentifier;
+    });
+  });
+
+  const y = tensorflow.oneHot(categoryIdentifiers, categories.length);
 
   return await graph.fit(x, y, {epochs: 2, verbose: 1});
 };
