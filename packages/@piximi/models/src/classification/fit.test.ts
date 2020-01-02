e0f902b937a26767ed7a3a21e0e8ffbab1970e4b
Allen Goodman <allen.goodman@icloud.com>
2020-01-02 14:32:18 -0500

    model functions

--- packages/@piximi/models/src/classification/fit.test.ts
+++ packages/@piximi/models/src/classification/fit.test.ts
@@ -1,3 +1,26 @@
-it("example", () => {
-  expect(1).toBe(1);
+import {compile} from "./compile";
+import {fit} from "./fit";
+import {mobilenetv1} from "./mobilenetv1";
+import {Loss, Metric, Optimizer} from "@piximi/types";
+
+const path =
+  "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json";
+
+describe("fit", () => {
+  it("metricsNames", async () => {
+    const promise = mobilenetv1(10, path, 100);
+
+    const options = {
+      learningRate: 0.01,
+      lossFunction: Loss.CategoricalCrossentropy,
+      metrics: [Metric.CategoricalAccuracy],
+      optimizationFunction: Optimizer.SGD
+    };
+
+    const graph = await compile(promise, options);
+
+    if (graph) {
+      expect(graph.metricsNames).toEqual(["loss", "categoricalAccuracy"]);
+    }
+  });
 });
