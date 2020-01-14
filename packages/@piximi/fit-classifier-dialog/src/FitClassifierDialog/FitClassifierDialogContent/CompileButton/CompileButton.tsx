92b5ac414a28bd2113051c61029ffff2f1d96074
Allen Goodman <allen.goodman@icloud.com>
2020-01-14 16:13:02 -0500

    CompileButton

--- packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/CompileButton/CompileButton.tsx
+++ packages/@piximi/fit-classifier-dialog/src/FitClassifierDialog/FitClassifierDialogContent/CompileButton/CompileButton.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
+import {useCallback} from "react";
 import {useStyles} from "./CompileButton.css";
 import Button from "@material-ui/core/Button/Button";
+import {useDispatch, useSelector} from "react-redux";
+import {open} from "@piximi/store";
+
+const pathname =
+  "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json";
 
 export const CompileButton = ({next}: {next: any}) => {
-  const classes = useStyles({});
+  const dispatch = useDispatch();
 
-  const onClick = () => {
+  const onClick = useCallback(() => {
+    dispatch(open({pathname: pathname, classes: 10, units: 100}));
+
     next();
-  };
+  }, [dispatch]);
+
+  const opened = useSelector(({classifier}) => classifier.model);
+
+  const classes = useStyles({});
 
   return (
     <Button
